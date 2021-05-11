79376869b30268e594b4d13744006716fa08442a
LAM <ngofungdaniel.lam@gmail.com>
2021-05-11 18:45:28 +0800

    working on inference script

--- GusarevModel.py
+++ GusarevModel.py
@@ -49,7 +49,9 @@ class Autoencoder(nn.Module):
         out = self.dec1(out)
         out = self.relu(out)
         return self.output_layer(out)
+####################
 # 6 Layer CNN Model
+####################
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, use_bias):
         super().__init__()
@@ -62,7 +64,6 @@ class ConvBlock(nn.Module):
         out = self.conv(x)
         out = self.norm(out)
         out = self.relu(out)
-        
         return out
 
 class MultilayerCNN(nn.Module):
@@ -94,4 +95,47 @@ class MultilayerCNN(nn.Module):
         out = self.layer4(out)
         out = self.layer5(out)
         out = self.conv_output(out)
+        return self.output_layer(out)
+
+#############
+# Interesting Saved Historical Models
+#############
+class MultilayerCNN_6LayerCNN_v2(nn.Module):
+    """
+    File: "./runs/6LayerCNN/v2/network_final.pt"
+    Input images [CxHxW] = 1x440x440
+    """
+    def __init__(self, input_array_size):
+        super().__init__()
+        print("Using 6-Layer MultiCNN Model.")
+        self.input_array_size = input_array_size
+        self.kernel_size = 5
+        self.stride = 1
+        self.padding = 2
+        self.use_bias = True
+        
+        in_nc = input_array_size[1]
+        out_nc = 16
+        self.layer1 = nn.Conv2d(in_nc, out_nc, self.kernel_size, self.stride, self.padding, bias=self.use_bias)
+        self.layer2 = nn.Conv2d(out_nc, out_nc*2, self.kernel_size, self.stride, self.padding, bias=self.use_bias)
+        self.layer3 = nn.Conv2d(out_nc*2, out_nc*4, self.kernel_size, self.stride, self.padding, bias=self.use_bias)
+        self.layer4 = nn.Conv2d(out_nc*4, out_nc*8, self.kernel_size, self.stride, self.padding, bias=self.use_bias)
+        self.layer5 = nn.Conv2d(out_nc*8, out_nc*16, self.kernel_size, self.stride, self.padding, bias=self.use_bias)
+        self.layer6 = nn.Conv2d(out_nc*16, in_nc, self.kernel_size, self.stride, self.padding, bias=self.use_bias)
+
+        self.relu = nn.ReLU()
+        # output layer
+        self.output_layer = nn.Sigmoid()
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.relu(out)
+        out = self.layer2(out)
+        out = self.relu(out)
+        out = self.layer3(out)
+        out = self.relu(out)
+        out = self.layer4(out)
+        out = self.relu(out)
+        out = self.layer5(out)
+        out = self.relu(out)
+        out = self.layer6(out)
         return self.output_layer(out)
